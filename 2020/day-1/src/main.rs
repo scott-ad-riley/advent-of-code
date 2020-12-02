c9cc7001da173b1e7d279c36f9d59999935f753b
Scott Riley <scott.riley111@gmail.com>
2020-12-02 14:22:00 +0000

    day 1 part 2 complete

--- 2020/day-1/src/main.rs
+++ 2020/day-1/src/main.rs
@@ -1,31 +1,31 @@
-use std::fs;
+use std::{collections::HashMap, fs};
 
 fn main() {
     let filename = "input.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    let items: Vec<(u32, u32)> = contents
+    let items = contents
         .split("\n")
-        .map(|row| row.parse::<u32>())
+        .map(|row| row.parse())
         .filter(|item| item.is_ok())
-        .map(|item| {
-            let unwrapped = item.unwrap();
-            (unwrapped, 2020 - unwrapped)
+        .map(|item| item.unwrap());
+
+    let mut sums_map: HashMap<u32, (u32, u32)> = HashMap::new();
+
+    items.clone().for_each(|number| {
+        items.clone().for_each(|other_number| {
+            if number != other_number && number + other_number < 2020 {
+                sums_map.insert(number + other_number, (number, other_number));
+            }
+
+            ()
         })
+    });
+
+    let result: Vec<&(u32, u32)> = items
+        .clone()
+        .filter_map(|number| sums_map.get(&(2020 - number)))
         .collect();
 
-    let result = items
-        .iter()
-        .find(|(_original, diff)| {
-            let result_match = items.iter().find(|&&each_item| diff == &each_item.0);
-
-            if result_match.is_some() {
-                true
-            } else {
-                false
-            }
-        })
-        .unwrap();
-
-    println!("{:?}", result)
+    println!("{:?}", result);
 }
