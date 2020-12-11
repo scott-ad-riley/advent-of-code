0be8023e87cdf14411eb562ca4eefcc23a6db2ed
Scott Riley <scott.riley111@gmail.com>
2020-12-11 13:31:51 +0000

    solves day 7 part 1 using better approach

--- 2020/day-7/src/main.rs
+++ 2020/day-7/src/main.rs
@@ -8,21 +8,23 @@ use std::collections::{HashMap, HashSet};
 use std::fs;
 
 type Color = String;
+type ContainedIn = HashMap<Color, HashSet<Color>>;
 
+#[derive(Debug)]
 struct Contained {
     count: usize,
-    name: Color,
+    color: Color,
 }
 
 fn main() {
     let filename = "input.txt";
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
 
-    // this is a map of Color -> Color that bag contains
-    let mut bags: HashMap<String, Vec<(usize, &str)>> = HashMap::new();
+    // this is a map of Color -> Contained where the key is the container, and the value is a Vec of what's contained inside it
+    let mut bags: HashMap<String, Vec<Contained>> = HashMap::new();
 
-    // this is a map of Color -> Color that contains that bag
-    let mut inverted_bags: HashMap<Color, Vec<Color>> = HashMap::new();
+    // this is a map of Color -> Set<Color> where the key is a child bag, and the value is a Set of of which bags contain it
+    let mut inverted_bags: HashMap<Color, HashSet<Color>> = HashMap::new();
 
     contents
         .split("\n")
@@ -30,27 +32,61 @@ fn main() {
         .map(|row| {
             let mut items = row.split("contain");
             let bag_type = extract_bag_type(items.next().unwrap());
-            let contents: Vec<(usize, &str)> = extract_contents(items.next().unwrap());
+            let contents: Vec<Contained> = extract_contents(items.next().unwrap());
             bags.insert(bag_type.clone().to_string(), contents);
 
             bag_type
         })
         .collect_vec();
 
-    println!("{:?}", bags);
+    for (bag, outer_contents) in bags {
+        for contents in outer_contents {
+            let entry = inverted_bags
+                .entry(contents.color)
+                .or_insert(HashSet::new());
+            entry.insert(bag.to_string());
+        }
+    }
 
-    let mut counter = 0;
+    let part1_answer = transitive_closure(inverted_bags, &"shiny gold".to_string()).len();
 
-    get_contained_bag_count("shiny gold".to_string(), &bags, &mut counter);
+    println!("{:?}", part1_answer);
+}
 
-    println!("{:?}", counter);
+fn transitive_closure(inverted_bags: ContainedIn, start: &Color) -> HashSet<Color> {
+    let mut valid_containers = HashSet::new();
+    calculate_transitive_closure_step(&inverted_bags, &mut valid_containers, vec![start]);
+
+    valid_containers.remove(start);
+    valid_containers
+}
+
+fn calculate_transitive_closure_step(
+    inverted_bags: &ContainedIn,
+    acc: &mut HashSet<Color>,
+    target: Vec<&Color>,
+) {
+    for color in target.iter() {
+        // check we haven't already found the containers for this bag
+        if !acc.contains(*color) {
+            // we want to find all the places that lead to color
+            acc.insert(color.to_string());
+            if let Some(new_containers) = inverted_bags.get(*color) {
+                calculate_transitive_closure_step(
+                    inverted_bags,
+                    acc,
+                    new_containers.iter().collect_vec(),
+                )
+            }
+        }
+    }
 }
 
 fn extract_bag_type(item: &str) -> String {
     item.trim_end().clone().replace(" bags", "")
 }
 
-fn extract_contents(item: &str) -> Vec<(usize, &str)> {
+fn extract_contents(item: &str) -> Vec<Contained> {
     lazy_static! {
         static ref CONTENTS_REGEX: Regex = Regex::new(r"([0-9]{1})\s(\w*\s\w*)\sbags?\.?").unwrap();
     }
@@ -58,51 +94,20 @@ fn extract_contents(item: &str) -> Vec<(usize, &str)> {
         .split(",")
         .filter(|x| !x.contains("no other bags"))
         .map(|x| {
-            CONTENTS_REGEX
-                .captures_iter(x)
-                .map(|capture| {
-                    (
-                        capture.get(1).unwrap().as_str().parse().unwrap(),
-                        capture.get(2).unwrap().as_str(),
-                    )
-                })
-                .nth(0)
-                .unwrap()
+            if let Some(capture) = CONTENTS_REGEX.captures_iter(x).next() {
+                if let (Some(count), Some(color)) = (capture.get(1), capture.get(2)) {
+                    let contained = Contained {
+                        color: color.as_str().to_string(),
+                        count: count.as_str().parse::<usize>().unwrap(),
+                    };
+
+                    contained
+                } else {
+                    panic!("Couldn't extract the parsed values from the line")
+                }
+            } else {
+                panic!("regex didn't match the line")
+            }
         })
         .collect()
-}
-
-fn get_contained_bag_count(
-    target_bag: String,
-    bag_type_to_contents_map: &HashMap<String, Vec<(usize, &str)>>,
-    acc: &mut usize,
-) -> usize {
-    let bags_inside_target = bag_type_to_contents_map.get(&target_bag).unwrap();
-
-    if bags_inside_target.len() == 0 {
-        println!("{:?} contains no bags (current acc:{:?})", target_bag, acc);
-        return 1;
-    }
-
-    bags_inside_target
-        .iter()
-        .map(|&(inner_bag_count, inner_bag_type)| {
-            let count = inner_bag_count
-                * get_contained_bag_count(
-                    inner_bag_type.to_string(),
-                    bag_type_to_contents_map,
-                    acc,
-                );
-
-            *acc += count;
-
-            println!(
-                "{:?} contains {:?}x{:?} bags (current total:{:?} just added:{:?})",
-                target_bag, inner_bag_count, inner_bag_type, acc, count
-            );
-
-            count
-        })
-        .inspect(|x| println!("summing: {:?}", x))
-        .sum()
 }
